493f146c12c0912810c7692e34ff0642d6c2d4b8
Levan Ilashvili <levaniko900@gmail.com>
2022-04-21 16:22:56 +0400

    add keyobard events

--- script.js
+++ script.js
@@ -38,14 +38,37 @@ numbers.forEach(number => {
     });
 });
 
-
-dot.addEventListener("click", () => {
+function dotHandler() {
     if(decimal === "") {
         currentNum += dot.textContent;
         display.textContent = currentNum;
         decimal = "."
+    }  
+}
+
+dot.addEventListener("click", dotHandler);
+
+function equalHandler() {
+    if(previousNum === "" && currentNum === "") {
+        display.textContent = "Input number"
+    } else {
+        previousNum = Number(previousNum);
+        currentNum = Number(currentNum);
+        console.log(operator, currentNum);
+        if(operator === "/" && currentNum == 0) {
+            display.textContent = "Bruh";
+            previousNum = "";
+            currentNum = "";
+            operator = "";
+        } else {
+            display.textContent = operate(operator, previousNum, currentNum).toFixed(2);
+            currentNum = operate(operator, previousNum, currentNum).toFixed(2);
+            operator = "";
+            }
     }
-})
+}
+
+equal.addEventListener("click", equalHandler);
 
 
 operators.forEach(op => {
@@ -73,30 +96,49 @@ operators.forEach(op => {
     });
 });
 
-equal.addEventListener("click", () =>{
-    if(previousNum === "" && currentNum === "") {
-        display.textContent = "Input number"
-    } else {
-        previousNum = Number(previousNum);
-        currentNum = Number(currentNum);
-        console.log(operator, currentNum);
-        if(operator === "/" && currentNum == 0) {
-            display.textContent = "Bruh";
-            previousNum = "";
-            currentNum = "";
-            operator = "";
-        } else {
-            display.textContent = operate(operator, previousNum, currentNum).toFixed(2);
-            currentNum = operate(operator, previousNum, currentNum).toFixed(2);
-            operator = "";
-            }
-    }
-    
-});
-
-clear.addEventListener("click", () => {
+function clearHandler() {
     previousNum = "";
     currentNum = "";
     operator = "";
     display.textContent = "";
-})
+}
+
+clear.addEventListener("click", clearHandler);
+
+
+document.addEventListener("keydown", (e) => {
+    const numbers = ['1','2','3','4','5','6','7','8','9','0'];
+    const operators = ['+','-','*','/'];
+    if(numbers.includes(e.key)) {
+        currentNum += e.key;
+        display.textContent = currentNum;
+    } else if(operators.includes(e.key)) {
+        if(operator !== "") {
+            if(operator === "/" && currentNum == 0) {
+                display.textContent = "Bruh";
+                previousNum = "";
+                currentNum = "";
+                operator = "";
+            } else {
+                previousNum = Number(previousNum);
+                currentNum = Number(currentNum);
+                display.textContent = operate(operator, previousNum, currentNum).toFixed(2);  
+                previousNum = operate(operator, previousNum, currentNum).toFixed(2);
+                currentNum = ""; 
+                operator = e.key;
+            }
+        } else {
+            previousNum = currentNum;
+            operator = e.key;
+            currentNum = "";
+        }
+        decimal = "";
+    } else if(e.key === '=' || e.key === 'Enter') {
+        equalHandler();
+    } else if(e.key === '.') {
+        dotHandler();
+    } else if(e.key === 'Backspace') {
+        clearHandler();
+    }
+});
+
